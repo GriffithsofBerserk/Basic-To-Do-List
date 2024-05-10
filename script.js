f8431667f7ea9e9db6e89aa404018e438da05ab6
Suat Can Ölçer <132690977+GriffithsofBerserk@users.noreply.github.com>
2024-05-10 18:29:20 +0300

    add and remove added

--- script.js
+++ script.js
@@ -1,42 +1,29 @@
 'use strict';
 
-//<li class="list-group-item">
-//<input class="form-check-input me-1" type="checkbox" value="" id="firstCheckbox">
-//<label class="form-check-label" for="firstCheckbox">First checkbox</label>
-//</li>
+let taskDOM = document.querySelector('#task')
+let addDOM = document.querySelector('#liveToastBtn')
+let list = document.querySelector('#list')
 
-let userFormDOM = document.querySelector('#userForm')
-userFormDOM.addEventListener('submit',formHandler)
+addDOM.addEventListener('click',newElement)
 
-function formHandler(event){
+function newElement(event){
     event.preventDefault()
-    const WORK_NAME = document.querySelector('#workname')
-    console.log(WORK_NAME.value)
-    addItem(WORK_NAME.value)
-}
+    
+    if(taskDOM.value.trim() !== ""){
+        let li = document.createElement("li")
+        li.textContent = taskDOM.value
+        list.appendChild(li)
+        let spanDOM = document.createElement('span')
+        spanDOM.className ="close"
+        spanDOM.textContent = "x"
+        
 
-let userListDOM = document.querySelector('#userList')
-const addItem = (workname) => {
-    let liDOM = document.createElement('li')
-    liDOM.innerHTML = `
-        <input class="form-check-input me-1" type="checkbox" value="" id="firstCheckbox">
-        <label class="form-check-label justify-content-between" for="firstCheckbox">${workname}</label>
-        <button type="button" class="btn btn-outline-primary float-end" id="delete">Delete</button> 
-        `
-    liDOM.classList.add(
-        'list-group-item',  'align-items-center','justify-content-between'
-    )
-    userListDOM.append(liDOM)
-    return userListDOM = document.querySelector('#userList')
-}
+    spanDOM.addEventListener('click',function(){
+        li.remove()
+    })
 
-let deleteText = document.querySelector('#userList')
-deleteText.addEventListener('click',formDelete)
-
-function formDelete(event){
-    event.preventDefault()
-    const DELETE_ITEM = document.querySelector('#delete')
-    console.log("delete tıklandı")
-    deleteText.remove(DELETE_ITEM.value)
-    return userListDOM = document.querySelector('#userList')
+    li.appendChild(spanDOM)
+    list.appendChild(li)
+    }
+    taskDOM.value =""
 }
