fb3c3fac709aa51aeeb07798394b4e25f76f6548
Suat Can Ölçer <132690977+GriffithsofBerserk@users.noreply.github.com>
2024-05-11 13:29:09 +0300

    localStorage

--- script.js
+++ script.js
@@ -4,38 +4,57 @@ let taskDOM = document.querySelector('#task')
 let addDOM = document.querySelector('#liveToastBtn')
 let list = document.querySelector('#list')
 let toastDOM = document.querySelector('#liveToast')
+let inputs = []
 
-addDOM.addEventListener('click',newElement)
+addDOM.addEventListener('click', newElement)
 
-function newElement(event){
+function newElement(event) {
     event.preventDefault()
     
-    if(taskDOM.value.trim() !== ""){
+    if (taskDOM.value.trim() !== "") {
         let li = document.createElement("li")
         li.textContent = taskDOM.value
         list.appendChild(li)
         let spanDOM = document.createElement('span')
         spanDOM.className ="close"
         spanDOM.textContent = "x"
-        
 
-    spanDOM.addEventListener('click',function(){
-        li.remove()
-    })
+        inputs.push(taskDOM.value)
+        console.log(inputs)
+        let inputString = JSON.stringify(inputs)
+        localStorage.setItem("inputs", inputString)
 
-    li.appendChild(spanDOM)
-    list.appendChild(li)
-    $(".success").toast("show");
-    }else{
-        $(".error").toast("show");
+        spanDOM.addEventListener('click', function() {
+            li.remove()
+        });
+
+        li.appendChild(spanDOM)
+        list.appendChild(li)
+        $(".success").toast("show")
+    } else {
+        $(".error").toast("show")
     }
     taskDOM.value =""
 }
 
-list.addEventListener("click", function(task) 
-{
-  if (task.target.tagName === "LI") 
-  {
-    task.target.classList.toggle("checked");
-  }
-});
+list.addEventListener("click", function(task) {
+    if (task.target.tagName === "LI") {
+        task.target.classList.toggle("checked")
+    }
+})
+
+window.onload = function() {
+    let storedInputs = localStorage.getItem("inputs")
+    if (storedInputs) {
+        inputs = JSON.parse(storedInputs)
+        inputs.forEach(function(task) {
+            let li = document.createElement("li")
+            li.textContent = task
+            let spanDOM = document.createElement("span")
+            spanDOM.className = "close"
+            spanDOM.textContent = "x"
+            li.appendChild(spanDOM)
+            list.appendChild(li)
+        })
+    }
+}
